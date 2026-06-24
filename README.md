189962e798ff0d4471cc51ba9dd5982ebce225ef
Руслан Тагиев <rtagiev@hyper.dthl>
2026-06-24 17:35:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Описание
+Скрипт для создания виртуальных машин на базе QEMU/KVM
+
+# Зависимости
+Помимо настроенного гипервизора(libvirtd демон) необходимы следующие пакеты:
+
+- qemu-utils
+- cloud-image-utils
+- virtinst
+
+```
+#Ubuntu/Debian
+apt update && apt install -y qemu-utils cloud-image-utils virtinst
+```
+# Первый запуск
+1. Загрузить скрипт
+```
+git clone https://github.com/tagiev001/create_vm.git
+```
+
+2. Подготовить соотвествующие user-data.yaml, meta-data.yaml, network-config.yaml,
+   в network-config.yaml указать интерфейс в зависимости от образа, указать в **CLOUD_INIT_CONFIG_PATHS** пути до соответствующих файлов
+
+3. Указать параметры ВМ в create_vm.py
+
+| Параметр | Описание | Тип |
+|-|-|-|
+| VM_NAME| Имя вм в virsh | str |
+| VM_RAM | Кол-во озу вм, гб | int |
+| VM_VCPUS | Кол-во vCPU | int |
+| VN_NET | Сеть для вм* | str |
+| VM_SIZE | Размер ВД, гб | int |
+| IMAGE_LINK | Ссылка на generic-cloud образ | str |
+| IMAGE_PATH | Место хранения ВМ | str |
+
+*Сеть для VM_NET должна быть определена до скрипта(virsh net-list --all)
+
+4. В **EXEC_VIRT_INSTALL_CMD** указать соотвествующий дистрибутив для параметра --os-variant**
+
+**Проверить список доступных значений можно с помощью `virt-install --osinfo list`
+
+5. Запустить скрипт
+```
+./create_vm.py
+```
+
+5. Подождать, пока cloud-init завершит настройку
+
+
+
+# Скрипт проверялся на 
+| Дистрибутив | ссылка на образ | Сетевой интерфейс(для network-config) |
+| -|-|-|
+| Debian13 | https://cloud.debian.org/images/cloud/trixie/20250806-2196/debian-13-genericcloud-amd64-20250806-2196.qcow2 | enp1s0 |
+| Almalinux8 | https://repo.almalinux.org/almalinux/8/cloud/x86_64/images/AlmaLinux-8-GenericCloud-ext4-8.10-20260518.x86_64.qcow2 | eth0 |
+
+P.S. Скорее всего он работает со всеми generic-cloud образами, проблема только в том, применятся ли network-config.yaml 
+
+# To do
+- Автоопределение дистрибутива для --os-variant
+- Указание  входящих параметров
+- Установка зависимостей
+- Автоопределение ВМ в сеть default при отсутствии указанной/ВМ без сети
+- Указание пула для виртуального диска ВМ
---
+++ create_vm.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+import subprocess
+import os
+import urllib.request
+import json
+
+
+
+def exec_command(args):
+    res = subprocess.run(args,capture_output=True,text=True)
+    return res.returncode, res.stdout
+
+def download_image(image_link, image_path, image_name):
+    print("Downloading cloudimage...")
+    def progress(block_num, block_size,total_size):
+        downloaded = block_num * block_size
+        print(f"\r{downloaded} / {total_size}", end="",flush=True)
+    urllib.request.urlretrieve(image_link, image_path, reporthook=progress)
+    filepath = os.path.abspath(image_name)
+    print("\nDone! Image path:", filepath)
+
+
+def check_configs(udp, mdp, ncp):
+    config_files = [udp, mdp, ncp]
+    for file in config_files:
+        if os.path.isfile(file) == False:
+            print(f"something wrong with {file}. Check is exists, perms")
+            return 1
+    print("Configs acceptable")
+
+
+def create_seed_img(userdata_path, metadata_path, networkconfig_path):
+    EXEC_SEED_CMD = ["cloud-localds", SEED_IMAGE_PATH, userdata_path, metadata_path, "--network-config", networkconfig_path]
+    check_configs(userdata_path, metadata_path, networkconfig_path)
+    print("Creating seed image with cloud-init configs....")
+    exec_command(EXEC_SEED_CMD)
+    filepath = os.path.abspath(SEED_IMAGE_PATH)
+    print("Done! Seed image path:", filepath)
+
+
+def resize_image(image_path, image_size):
+    EXEC_QEMUIMG_CMD = ["qemu-img", "info", "--output=json", image_path]
+    size = exec_command(EXEC_QEMUIMG_CMD)[1]
+    virtual_size = json.loads(size)["virtual-size"]
+    how_many_add_to_image = image_size*1024*1024*1024 - int(virtual_size)
+    EXEC_RESIZE_CMD = ["qemu-img" , "resize", image_path, f"+{how_many_add_to_image}"]
+    exec_command(EXEC_RESIZE_CMD)
+
+
+def create_virtual_machine(vm_name, vm_ram, vm_vcpus, vm_net, vm_disk):
+    print("Creating VM...")
+
+    EXEC_VIRT_INSTALL_CMD = [
+        "virt-install",
+        "--name", vm_name,
+        "--memory", f"{vm_ram*1024}",
+        "--vcpus", str(vm_vcpus),
+        "--disk", str(vm_disk),
+        "--disk", f"{SEED_IMAGE_PATH},device=cdrom",
+        "--import",
+        "--os-variant", "almalinux8",
+        "--network", f"network={vm_net}",
+        "--graphics", "vnc",
+        "--noautoconsole",
+        "--wait", "0"
+    ]
+
+    exec_command(EXEC_VIRT_INSTALL_CMD)
+    print(f"{vm_name} created! Wait, while cloud-init is working")
+
+def show_virtual_machine_info():
+    pass
+
+def is_cloudinit_finished():
+    pass
+
+def create_network():
+    pass
+
+def install_deps():
+    pass
+
+
+VM_NAME = "test-vm"
+VM_RAM = 2
+VM_VCPUS = 2
+VM_NET = "homelab"
+VM_SIZE = 15
+
+
+IMAGE_LINK = "https://cloud.debian.org/images/cloud/trixie/20250806-2196/debian-13-genericcloud-amd64-20250806-2196.qcow2"
+IMAGE_LINK = "https://repo.almalinux.org/almalinux/8/cloud/x86_64/images/AlmaLinux-8-GenericCloud-ext4-8.10-20260518.x86_64.qcow2"
+IMAGE_NAME = f"{VM_NAME}.qcow2"
+IMAGE_PATH = f"/var/lib/libvirt/images/{IMAGE_NAME}"
+
+SEED_IMAGE_PATH = f"/var/lib/libvirt/images/{VM_NAME}_seed.img"
+
+CLOUD_INIT_CONFIGS_PATHS = {
+    "user-data":"user-data.yaml",
+    "meta-data":"meta-data.yaml",
+    "network-config":"network-config.yaml"
+}
+
+
+
+
+if __name__ == "__main__":
+    download_image(IMAGE_LINK,IMAGE_PATH,IMAGE_NAME)
+
+    create_seed_img(CLOUD_INIT_CONFIGS_PATHS["user-data"],
+                    CLOUD_INIT_CONFIGS_PATHS["meta-data"],
+                    CLOUD_INIT_CONFIGS_PATHS["network-config"])
+    resize_image(IMAGE_PATH, VM_SIZE)
+    create_virtual_machine(VM_NAME, VM_RAM, VM_VCPUS, VM_NET, IMAGE_PATH)
